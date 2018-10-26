2eb061dbf7db2e6d6af2069b72f5af135035998c
ralfsantacruz <rsantac1@gmail.com>
2018-10-26 14:30:16 -0700

    added starter code

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,13 @@
+// from data.js
+var tableData = data;
+// print out the table data so we can see it
+console.log(tableData)
+
+tbody = d3.select("tbody")
+
+// print out each UFO sighting object
+tableData.forEach(function(data) {
+    console.log(data);
+})
+
+// YOUR CODE HERE!
