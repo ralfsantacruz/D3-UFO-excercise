4d779d0436010a6b1c7dd639c12595890a5e5b78
ralfsantacruz <rsantac1@gmail.com>
2018-10-29 13:01:14 -0700

    fixed bugs and fine-tuned filter

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,70 +1,48 @@
 // from data.js
 var tableData = data;
 
+var textBox = d3.select("input")
+var button = d3.select("#filter-btn")
 
-textBox = d3.select("input")
-button = d3.select("#filter-btn")
-
-
+// appends info to table on webpage
 function appendTable(table) {
     tableBody = d3.select("tbody")
     table.forEach(function(item) {
         tableBody.append("tr")
         Object.entries(item).forEach(function([key, value]) {
-            console.log(key, value)
             tableBody.append("td").text(value) 
         })
     })
 }
 
-
-
-
-
-// append default information to the table
-// tableData.forEach(function(data) {
-//     tableBody.append("tr")
-//     Object.entries(data).forEach(function([key, value]) {
-//         console.log(key, value)
-//         tableBody.append("td").text(value)
-//     })
-// })
-
+// Load up all the data in data.js as default
 appendTable(tableData)
 
-
-
-
-// create an action that occurs when the filter button is clicked
+// Create an action that occurs when the filter button is clicked
 button.on("click", function() {
     d3.event.preventDefault();
     inputText = d3.event.target.value
 
-    returnValue = textBox.property("value")
-   
+    // Get value from input box and convert to lowercase
+    returnValue = textBox.property("value").toLowerCase()
+
+    // Clear table if there is data
     if (tableBody) {
         d3.select("tbody").remove("td")
     }
-
+    // Re-append a new, blank table
     tbody = d3.select("table").append("tbody")
 
+    // Get filter criteria from dropdown menu
     selection = d3.select("#d3-dropdown").node().value;
-
-    
-    filteredTable = tableData.filter(stuff => stuff[selection] == returnValue)
-
     // console.log(selection)
-    // console.log(returnValue)
-    // console.log(filteredTable)
-    appendTable(filteredTable)
-    
-    // filteredTable.forEach(function(item) {
-    //     tbody.append("tr")
-    //     Object.entries(item).forEach(function([key, value]) {
-    //         console.log(key, value)
-    //         tbody.append("td").text(value) 
-    //     })
-    // })
+    if (selection === "showAll") {
+        appendTable(tableData)
+    }
+    else {
+        filteredTable = tableData.filter(object => object[selection] == returnValue)
+        appendTable(filteredTable)
+    }
 })
 
 
