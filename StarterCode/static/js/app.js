28d3f033a790faf9f35df843d483b43c2508f36b
ralfsantacruz <rsantac1@gmail.com>
2018-10-29 12:20:15 -0700

    added dropdown menu for filtering

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,13 +1,70 @@
 // from data.js
 var tableData = data;
-// print out the table data so we can see it
-console.log(tableData)
 
-tbody = d3.select("tbody")
 
-// print out each UFO sighting object
-tableData.forEach(function(data) {
-    console.log(data);
+textBox = d3.select("input")
+button = d3.select("#filter-btn")
+
+
+function appendTable(table) {
+    tableBody = d3.select("tbody")
+    table.forEach(function(item) {
+        tableBody.append("tr")
+        Object.entries(item).forEach(function([key, value]) {
+            console.log(key, value)
+            tableBody.append("td").text(value) 
+        })
+    })
+}
+
+
+
+
+
+// append default information to the table
+// tableData.forEach(function(data) {
+//     tableBody.append("tr")
+//     Object.entries(data).forEach(function([key, value]) {
+//         console.log(key, value)
+//         tableBody.append("td").text(value)
+//     })
+// })
+
+appendTable(tableData)
+
+
+
+
+// create an action that occurs when the filter button is clicked
+button.on("click", function() {
+    d3.event.preventDefault();
+    inputText = d3.event.target.value
+
+    returnValue = textBox.property("value")
+   
+    if (tableBody) {
+        d3.select("tbody").remove("td")
+    }
+
+    tbody = d3.select("table").append("tbody")
+
+    selection = d3.select("#d3-dropdown").node().value;
+
+    
+    filteredTable = tableData.filter(stuff => stuff[selection] == returnValue)
+
+    // console.log(selection)
+    // console.log(returnValue)
+    // console.log(filteredTable)
+    appendTable(filteredTable)
+    
+    // filteredTable.forEach(function(item) {
+    //     tbody.append("tr")
+    //     Object.entries(item).forEach(function([key, value]) {
+    //         console.log(key, value)
+    //         tbody.append("td").text(value) 
+    //     })
+    // })
 })
 
-// YOUR CODE HERE!
+
